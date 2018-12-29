821ae25a6988d0e3176c3ac7bd63a539ceae7de7
bankjoly <44179527+bankjoly@users.noreply.github.com>
2018-12-29 12:16:51 -0500

    Add files via upload

---
+++ port.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+    header();
+footer();
+
+});
+//adds styled text to header
+function header(){
+
+    $("#head").html("<span ><b>Joel Torney's Portfolio</b></span>");
+}
+
+function footer(){
+    //wanted to add links to other pages but they didnt work
+    // $("#foot").html(' <a href="C:\Users\joelt\Documents\PORTFOLIO\portfolio.html">Portfolio</a>' +
+    // '<a href="C:\Users\joelt\Documents\PORTFOLIO\contact.html"> Contact</a>');
+}
